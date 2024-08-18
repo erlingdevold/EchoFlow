f152da27463213df5d4b0e39bfaf7c87e6b98136
Erling Devold <erling.devold@sintef.no>
2024-08-18 22:11:35 +0200

    :bug: add entrypoints.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ echo "Output directory: $OUTPUT_DIR"
 
 ls -l $INPUT_DIR
 ls -l $OUTPUT_DIR
-# Then you can use these variables in your script
-# python app.py --input $INPUT_DIR --output $OUTPUT_DIR
 python main.py
+
 # when done, watch fs to see if changes happen.
 python stat.py
